5994ea0d0ed1f740dc44d287d7cef62ddb259dd0
zeein <zeein81@gmail.com>
2024-08-16 10:24:30 +0900

    hero > mobile bg

--- template-parts/section-hero.php
+++ template-parts/section-hero.php
@@ -1,9 +1,22 @@
 <?php
 $hero = get_field('hero');
 $bg = $hero['bg']['url'];
+$bgMobile = @$hero['bgMobile']['url'];
+$sayBg =
+  '<div class="hero--bg-pc" style="background-image:url(' . $bg . ')"></div>';
+if ($bgMobile) {
+  $sayBg =
+    '
+    <div class="hero--bg-pc hidden lg:block" style="background-image:url(' .
+    $bg .
+    ')"></div>
+    <div class="hero--bg-mo block lg:hidden" style="background-image:url(' .
+    $bgMobile .
+    ')"></div>';
+}
 ?>
 <section class="hero bg-black">
-  <div class="hero--bg" style="background-image: url('<?= $bg ?>');"></div>
+  <div class="hero--bg"><?= $sayBg ?></div>
   <div class="hero--slogan">
     <div class="title">
       <div class="title-up"><?= $hero['titleHeader'] ?></div>
